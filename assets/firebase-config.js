7e8b5384cacae516cbe41d829ddb93c05b986840
Arul Murugan B <123822677+Arul-AM@users.noreply.github.com>
2026-09-26 11:50:38 +0530

    34

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,20 +1,10 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.ASO_FIREBASE_CONFIG = {
   apiKey: "AIzaSyA8rJlEFvFPHjiax3tjOJN5t9zBx64_wJw",
   authDomain: "a-s-o-1a347.firebaseapp.com",
   projectId: "a-s-o-1a347",
   storageBucket: "a-s-o-1a347.firebasestorage.app",
   messagingSenderId: "258762368777",
   appId: "1:258762368777:web:a6115f2594445209f115d8",
-  measurementId: "G-YPMD37DBY1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+window.ASO_FUNCTIONS_BASE = "https://REGION-YOUR-PROJECT-ID.cloudfunctions.net";
